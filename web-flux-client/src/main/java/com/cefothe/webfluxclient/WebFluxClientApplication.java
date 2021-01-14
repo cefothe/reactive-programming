5e9ee6adef37aba83cf320bd72000fcb81a18d6c
stefanangelovMM <stefanangelov@mentormate.com>
2021-01-14 08:32:59 +0200

    use latest version on lombok provided by spring boot management and use JUnit 5

--- web-flux-client/src/main/java/com/cefothe/webfluxclient/WebFluxClientApplication.java
+++ web-flux-client/src/main/java/com/cefothe/webfluxclient/WebFluxClientApplication.java
@@ -1,15 +1,11 @@
 package com.cefothe.webfluxclient;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Date;
 
 @SpringBootApplication
 public class WebFluxClientApplication {
